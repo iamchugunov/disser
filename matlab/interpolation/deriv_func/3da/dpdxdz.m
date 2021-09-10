01723b0d1a79d684fc65d24e8b32df2921d93d76
Alexander Chugunov <64365983+iamchugunov@users.noreply.github.com>
2021-09-10 15:55:51 +0300

    Исправил ошибку 3\2

--- matlab/interpolation/deriv_func/3da/dpdxdz.m
+++ matlab/interpolation/deriv_func/3da/dpdxdz.m
@@ -2,10 +2,10 @@ function [f] = dpdxdz(y,x,t,i,j, config)
     Sn = config.sigma_n_ns;
     X = config.posts;
     % производная частного (первое слагаемое в dp/dx
-    f1 = - 1/Sn^2 * y * 1/config.c_ns * (x_t(x,t) - X(1,i))*(z_t(x,t) - X(3,i))/R_t(x, t, X(:,i))^3/2;
+    f1 = - 1/Sn^2 * y * 1/config.c_ns * (x_t(x,t) - X(1,i))*(z_t(x,t) - X(3,i))/R_t(x, t, X(:,i))^(3/2);
     % производная произведения (второе слагаемое слагаемое в dp/dx
     % сначала произодная частного во втором множителе
-    f3 = - (x_t(x,t) - X(1,i))*(z_t(x,t) - X(3,i))/R_t(x, t, X(:,i))^3/2;
+    f3 = - (x_t(x,t) - X(1,i))*(z_t(x,t) - X(3,i))/R_t(x, t, X(:,i))^(3/2);
     f2 = 1/Sn^2 * 1/config.c_ns * ( 1/config.c_ns * (z_t(x,t) - X(3,i))/R_t(x, t, X(:,i)) * (x_t(x,t) - X(1,i))/R_t(x, t, X(:,i)) + S_t(x, t, i, j, config) * f3);
     f = f1 - f2;
 end
